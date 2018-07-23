8222cc4f613a23896199096a54800a011b68798b
LonelyLiaR <lonelyliar8523@gmail.com>
2018-07-23 15:32:33 +0800

    Modified HomeLoader loading position.

--- src/components/HomePage/loader.jsx
+++ src/components/HomePage/loader.jsx
@@ -7,14 +7,14 @@ const Rects = () => (
   <Fragment>
     <rect
       x={clientWidth / 2 - 72}
-      y="130"
+      y={clientHeight / 2 - 196}
       rx="144"
       ry="144"
       width="144"
       height="144"
     />
-    <rect x={clientWidth / 2 - 64} y="304" width="128" height="22" />
-    <rect x={clientWidth / 2 - 150} y="344" width="300" height="18" />
+    <rect x={clientWidth / 2 - 64} y={clientHeight / 2 - 26} width="128" height="22" />
+    <rect x={clientWidth / 2 - 150} y={clientHeight / 2 + 16} width="300" height="18" />
   </Fragment>
 );
 
